8b8df67375ceb999a70fcbd5de681f69e6f24b69
clecap <clemens.cap@uni-rostock.de>
2023-09-01 16:59:56 +0200

    automatically generated

--- install-dante.sh
+++ install-dante.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-
 echo "*** Making a backup of configuration file"
 cp CONF.sh CONF-backup.sh
+echo "DONE making a backup of configuration file";
 
 echo "*** Pulling Docker Images from docker hub..."
-  docker pull clecap/my-lap-container
+  docker pull clecap/my-lap
   docker pull clecap/my-mysql
 echo "DONE pulling docker images"
 
+
+
+# images-my
+
+# ${DIR}/../../my-mysql/bin/run.sh ${DB_SPEC}
+# images-lap-bin-run.sh
+# ["--db", "my-test-db-volume",  "--dir", "full"]
 
 # docker run
 # docker run
