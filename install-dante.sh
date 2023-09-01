8588f7e6dd8e9b1dad9d155f47876a518a692b68
clecap <clemens.cap@uni-rostock.de>
2023-09-01 17:34:41 +0200

    Not a commit message

--- install-dante.sh
+++ install-dante.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
-echo "*** Making a backup of configuration file"
+echo "*** Making a backup of the configuration file CONF.sh"
 cp CONF.sh CONF-backup.sh
-echo "DONE making a backup of configuration file";
+echo "DONE making a backup of the configuration file CONF.sh";
 
 echo "*** Pulling Docker Images from docker hub..."
   docker pull clecap/my-lap
   docker pull clecap/my-mysql
 echo "DONE pulling docker images"
 
-
-
-# images-my
-
-# ${DIR}/../../my-mysql/bin/run.sh ${DB_SPEC}
-# images-lap-bin-run.sh
-# ["--db", "my-test-db-volume",  "--dir", "full"]
-
-# docker run
-# docker run
-
-
+echo "*** Starting containers..."
+images/lap/bin/both.sh --db my-test-db-volume --dir full
+echo "DONE starting containers"
 
 echo "*** Building volume"
+mkdir -p volumes/full/content/wiki-dir
+tar -xzvf dante-deploy.tar.gz  -C volumes/full/content/wiki-dir
+echo "DONE building volume"
 
+echo "*** Generating configuration file directory"
+mkdir -p conf
+source CONF.sh
 
+MWP=conf/mediawiki-PRIVATE.php
+
+rm -f ${MWP}
+echo  "<?php \n"   > ${MWP}
+echo "$wgPasswordSender='${SMTP_SENDER_ADDRESS}';          // address of the sending email account                            " >> ${MWP}
+echo "$wgSMTP = [                                                                                                             " >> ${MWP}
+echo  "  'host'     => '${SMTP_HOSTNAME}',                 // hostname of the smtp server of the email account  " >> ${MWP}
+echo  "  'IDHost'   => 'localhost',                        // sub(domain) of your wiki                                             " >> ${MWP}
+echo  "  'port'     => ${SMTP_PORT},                       // SMTP port to be used      " >> ${MWP}
+echo  "  'username' => '${SMTP_USERNAME}',                 // username of the email account   " >> ${MWP}
+echo  "  'password' => '${SMTP_PASSWORD}',                 // password of the email account   " >> ${MWP}
+echo  "  'auth'     => true                                // shall authentisation be used    " >> ${MWP}
+echo "]; ?>  " >> ${MWP}
 
 echo "*** Initializing Database"
 
-
 echo "*** Loading initial content"
-
-
--- update-dante.sh
+++ update-dante.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "*** Getting fresh source..."
 wget https://github.com/clecap/dante-wiki-production/archive/refs/heads/main.zip
+echo "DONE getting fresh source"
+
+echo "*** Unzipping source..."
 unzip -j main.zip
+echo "DONE unzipping fresh source"
+
+echo "*** Copying in preserved old configuration..."
 cp -f CONF-backup.sh CONF.sh
+echo "DONE copying in preserved old configuration"
+
