8dfb5ef6bf6e70903dea387eb5cd53da91d2928a
clecap <clemens.cap@uni-rostock.de>
2023-09-03 13:41:20 +0200

    automatically generated

--- volumes/full/spec/script-library.sh
+++ volumes/full/spec/script-library.sh
@@ -29,7 +29,7 @@ dropUser () {
   printf "\n\n*** dropUser: Dropping default anonymous user \n"
 
 
-# DROP USER ‘’@’localhost’, ‘’@’${DB_CONTAINER}’, ´username´@´localhost´;
+# DROP USER ''@'localhost', ‘’@'${DB_CONTAINER}', 'username'@'localhost';
 
   docker exec -i ${DB_CONTAINER} mysql -u root --password=${MYSQL_ROOT_PASSWORD} <<MYSQLSTUFF
 DROP USER ''@'localhost';
