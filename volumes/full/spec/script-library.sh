75bb6e2443916ca156f45f4fc8bf7410cb137cb9
clecap <clemens.cap@uni-rostock.de>
2023-09-03 13:45:31 +0200

    automatically generated

--- volumes/full/spec/script-library.sh
+++ volumes/full/spec/script-library.sh
@@ -32,7 +32,8 @@ dropUser () {
 # DROP USER ''@'localhost', ‘’@'${DB_CONTAINER}', 'username'@'localhost';
 
   docker exec -i ${DB_CONTAINER} mysql -u root --password=${MYSQL_ROOT_PASSWORD} <<MYSQLSTUFF
-DROP USER ''@'localhost';
+DROP USER IF EXISTS ''@'localhost';
+DROP USER IF EXISTS ''@'{DB_CONTAINER}';
 SELECT user, host, password from mysql.user;
 MYSQLSTUFF
 
