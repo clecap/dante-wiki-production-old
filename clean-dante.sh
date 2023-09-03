7b0ee939815eb653d6010e9ce9abb513b6f3b407
clecap <clemens.cap@uni-rostock.de>
2023-09-03 11:44:56 +0200

    Not a commit message

--- clean-dante.sh
+++ clean-dante.sh
@@ -2,19 +2,94 @@
 
 # Cleaning up a dante installation
 
+## NOTE: The original of this file currently is in dante-wiki-production
+
 # get directory where this script resides wherever it is called from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "*** Cleaning up docker ressources generated..."
+
+usage() {
+  echo "Usage: $0   and add one or more options on what to clean"
+  echo "Clean files:        --files         "
+  echo "Clean volumes:      --volumes       "
+  echo "Clean containers:   --containers       "
+  echo "Clean images:       --images        "
+  echo "Clean networks:     --networks     "
+  echo "Clean all:          --all          "
+ exit 1
+}
+
+
+##
+## Parse command line
+##
+# region
+if [ "$#" -eq 0 ]; then
+  usage
+else 
+  while (($#)); do
+    case $1 in 
+      (--files) 
+         cleanFiles;;
+      (--volumes) 
+        cleanVolumes;;
+      (--ontainers)
+        cleanContainers;;
+      (--images)
+        cleanImages;; 
+      (--networks)
+        cleanNetworks;;
+      (--all)
+        cleanAll;;
+      (*) 
+         usage
+         exit 1;;
+    esac
+    shift 1
+  done
+fi
+
+
+cleanFiles () {
+  echo "*** Removing generated directories"
+  rm -Rf ${DIR}/volumes
+  rm -Rf ${DIR}/conf
+echo "DONE removing generated directories"
+}
+
+
+
+cleanContainers () {
+  echo "*** Stopping and removing docker containers"
   docker container stop my-lap-container -t 0
   docker container rm my-lap-container
   docker container stop my-mysql -t 0
   docker container rm my-mysql
-  docker network   rm dante-network
-  docker volume rm my-test-db-volume
-echo "DONE cleaning up docker ressources generated"
+  echo "DONE stopping and removing docker containers"
+}
 
-echo "*** Removing generated directories"
-  rm -Rf ${DIR}/volumes
-  rm -Rf ${DIR}/conf
-echo "DONE removing generated directories"
+cleanNetwork () {
+  echo "*** Cleaning up docker networks generated..."
+  docker network   rm dante-network
+  echo "DONE cleaning up docker networks generated"
+}
+
+cleanVolumes () {
+  echo "*** Cleaning up docker volumes generated..."
+  docker volume rm my-test-db-volume
+  docker volume rm sample-volume
+  echo "DONE cleaning up docker volumes generated"
+}
+
+
+
+
+
+
+echo "*** Displaying existing docker resources..."
+  docker container ls
+  docker network ls
+  docker image ls
+echo "DONE displaying docker resources\n"
+
+
