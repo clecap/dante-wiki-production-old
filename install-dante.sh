7d4b1e4f521da2d8490d8ef4d4208edfde37fa54
clecap <clemens.cap@uni-rostock.de>
2023-09-03 21:42:32 +0200

    Not a commit message

--- install-dante.sh
+++ install-dante.sh
@@ -25,7 +25,7 @@ docker volume create ${LAP_VOLUME}
 echo "DONE building docker volume"
 
 #  -rm  automagically remove container when it exits
-docker run --rm  --volume ${PWD}:/source -v ${LAP_VOLUME}:/dest -w /source alpine cp CONF.sh /dest
+docker run --rm  --volume ${PWD}:/source --volume ${LAP_VOLUME}:/dest -w /source alpine cp CONF.sh /dest
 
 # docker run --rm -volume $PWD:/ -volume ${LAP_VOLUME}:/var/www/html/wiki-dir alpine cp CONF.sh /dest
 
