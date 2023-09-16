695becd3746e91ce69ce253734582a603931aea9
clecap <clemens.cap@uni-rostock.de>
2023-09-17 00:39:04 +0200

    Not a commit message

--- initial-content.sh
+++ initial-content.sh
@@ -29,9 +29,13 @@ echo ""; echo "* Doing namespaces 10"
 docker exec  --user ${USER} ${LAP_CONTAINER}  php /var/www/html/wiki-dir/maintenance/importDump.php --namespaces '10' --debug ${DUMPFILE}
 echo "DONE namespaces 10"
 
-echo ""; echo "* Doing the rest" 
+echo ""; echo "* Doing the rest, but no uploads flag"
+docker exec  --user ${USER} ${LAP_CONTAINER}  php /var/www/html/wiki-dir/maintenance/importDump.php --debug ${DUMPFILE}
+echo "DONE rest, no upload flag"
+
+echo ""; echo "* Doing the upload flag" 
 docker exec  --user ${USER} ${LAP_CONTAINER}  php /var/www/html/wiki-dir/maintenance/importDump.php --uploads --debug ${DUMPFILE}
-echo "DONE the rest"
+echo "DONE the upload flag"
 
 echo ""; echo "*** Running some maintenance commands"
 docker exec  --user ${USER} ${LAP_CONTAINER}  php /var/www/html/wiki-dir/maintenance/rebuildrecentchanges.php
